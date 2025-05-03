556e247068367c98fb1ee50e21272f14b98c71cd
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-03 22:46:51 +0600

    40-2 Display Countries In A Simple Way, Folder Structure

--- Module-40 Simple React Rest Countries/react-world-tour/src/components/Countries/Countries.jsx
+++ Module-40 Simple React Rest Countries/react-world-tour/src/components/Countries/Countries.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Country from "../Country/Country";
 
 
 const Countries = () => {
@@ -12,7 +13,10 @@ const Countries = () => {
     }, [])
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries: {countries.length}</h3>
+            {
+                countries.map(country => <Country key={countries.toLocaleString.cca3} country={country}></Country>)
+            }
         </div>
     );
 };
