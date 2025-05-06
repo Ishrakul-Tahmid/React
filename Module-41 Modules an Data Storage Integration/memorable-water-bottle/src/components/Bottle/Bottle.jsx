e3964c59f3b8651a8fa41448ab6f9120b00519d0
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-06 12:41:37 +0600

    Create Shopping Cart To Handle Event With Parameters

--- Module-41 Modules an Data Storage Integration/memorable-water-bottle/src/components/Bottle/Bottle.jsx
+++ Module-41 Modules an Data Storage Integration/memorable-water-bottle/src/components/Bottle/Bottle.jsx
@@ -1,12 +1,13 @@
 import './Bottle.css'; // Assuming you have a CSS file for styling
 
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleAddToCart }) => {
     const { name, img, price } = bottle;
     return (
         <div className='bottle'>
             <h3>Bottle: {name}</h3>
             <img src={img} alt="Bottle Image" />
             <p>Price: {price}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Purchase</button>
         </div>
     );
 };
--- Module-41 Modules an Data Storage Integration/memorable-water-bottle/src/components/Bottles/Bottles.jsx
+++ Module-41 Modules an Data Storage Integration/memorable-water-bottle/src/components/Bottles/Bottles.jsx
@@ -4,18 +4,28 @@ import './Bottles.css';
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('bottles.json')
             .then(res => res.json())
             .then(data => setBottles(data))
     })
+
+    const handleAddToCart = (bottle) => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
+
     return (
         <div>
-            <h2>Bottles here: {bottles.length}</h2>
+            <h2>Bottles Available: {bottles.length}</h2>
+            <h3>Bottles in Cart: {cart.length}</h3>
             <div className="bottles-container">
                 {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}> </Bottle>)
+                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}
+                    handleAddToCart={handleAddToCart}
+                    > </Bottle>)
                 }
             </div>
         </div>
