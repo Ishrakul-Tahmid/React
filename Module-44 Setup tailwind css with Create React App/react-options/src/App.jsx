a03aaa757ddf42aba964008c9497ada4d3a1d9ea
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-11 11:57:53 +0600

    Axios, Data Extraction And Data Processing Bar Chart

--- Module-44 Setup tailwind css with Create React App/react-options/src/App.jsx
+++ Module-44 Setup tailwind css with Create React App/react-options/src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import DaisyNav from "./Components/DaisyNav/DaisyNav";
 import PriceOptions from "./Components/PriceOptions/PriceOptions";
 import LineChart  from "./Components/LineChart/LineChart";
+import Phones from "./Components/Phones/Phones";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -74,6 +75,7 @@ function App() {
       <PriceOptions></PriceOptions>
 
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
