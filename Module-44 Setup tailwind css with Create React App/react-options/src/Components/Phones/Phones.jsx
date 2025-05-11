decc18b22c36d8cbc29365786f282669233c9ef1
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-11 12:40:37 +0600

    Explore React Awesome Components

--- Module-44 Setup tailwind css with Create React App/react-options/src/Components/Phones/Phones.jsx
+++ Module-44 Setup tailwind css with Create React App/react-options/src/Components/Phones/Phones.jsx
@@ -1,9 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip } from "recharts";
+import {
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import { Audio } from "react-loader-spinner";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -24,16 +33,30 @@ const Phones = () => {
         });
         console.log(phonesWithFakeData);
         setPhones(phonesWithFakeData);
+        setLoading(false);
       });
   }, []);
   return (
     <div>
+      {loading && (
+        <div>
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+      )}
       <h2 className="text-5xl">Phones: {phones.length}</h2>
       <BarChart width={600} height={400} data={phones}>
         <Bar dataKey="price" fill="#8884d8" />
         <XAxis dataKey={"name"}></XAxis>
-              <YAxis></YAxis>
-              <Tooltip></Tooltip>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
       </BarChart>
     </div>
   );
