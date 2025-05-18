c920922c276bb4ee02d21d2963c53c8ea8e59dc9
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-18 12:16:20 +0600

    Explain Nested Route Concepts

---
+++ React Router and State Management/intro-to-react-router/src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            return(
+            <h2>Contact us</h2>
+            <p>Contact us at contact@example.com</p>
+            )
+        </div>
+    );
+};
+
+export default Contact;
