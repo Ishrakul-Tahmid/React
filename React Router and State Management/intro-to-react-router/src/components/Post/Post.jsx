c495a017048290efebd35fd1abad1298823f725d
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-18 23:07:29 +0600

    Navigate On Click, UseNavigate, Not Found And UseParams

--- React Router and State Management/intro-to-react-router/src/components/Post/Post.jsx
+++ React Router and State Management/intro-to-react-router/src/components/Post/Post.jsx
@@ -1,13 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Post = ({ post }) => {
     const { title, body, id } = post;
+
+    const navigaye = useNavigate();
+
     const userStyle = {
         border: '2px solid yellow',
         padding: '10px',
         borderRadius: '10px',
     }
+
+    const handleShowDetails = () => {
+        navigaye(`/post/${id}`);
+    }
+
     return (
         <div style={userStyle}>
             <h2>Post {id}</h2>
@@ -16,6 +24,7 @@ const Post = ({ post }) => {
             <Link to={`/post/${id}`}>
                 <button>Show Details</button>   
             </Link>
+            <button onClick={handleShowDetails}>Click to see details</button>
         </div>
     );
 };
--- React Router and State Management/intro-to-react-router/src/components/postDetails/postDetails.jsx
+++ React Router and State Management/intro-to-react-router/src/components/postDetails/postDetails.jsx
@@ -1,19 +1,29 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const navigate = useNavigate();
+    // const params = useParams();
+    const { postId } = useParams();
+    console.log(postId);
     const { id, title, body } = post;
     const userStyle = {
         border: '2px solid yellow',
         padding: '10px',
         borderRadius: '10px',
     }
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div style={userStyle}>
             <h2>Post {id}</h2>
             <h3>{title}</h3>
             <p>{body}</p>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
