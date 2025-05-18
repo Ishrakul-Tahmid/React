2234474abce00956fad123db2bc0edc5afea7cc7
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-18 10:00:56 +0600

    What Is SPA, Routing And React Router Setup

---
+++ React Router and State Management/intro-to-react-router/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello from router</div>
+  },
+  {
+    path: '/about',
+    element: <div>About</div>
+  },
+  {
+    path: '/contact',
+    element: <div>Contact</div>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
