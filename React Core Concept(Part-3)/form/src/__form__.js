183ccc63a89c630572fe79094d95bdd1088637ac
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-20 10:43:23 +0600

    Handle Form With OnSubmit And Access Field Data

---
+++ React Core Concept(Part-3)/form/src/__form__.js
@@ -0,0 +1,4 @@
+/*
+1: use form tag top get value
+
+*/
---
+++ React Core Concept(Part-3)/form/src/components/Simpleform/Simpleform.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Simpleform = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        console.log('Form submitted');
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name'></input>
+                <br></br>
+                <input type="email" name="email" id="" />
+                <br></br>
+                <input type="text" name="phone" id="" />
+                <br></br>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Simpleform;
